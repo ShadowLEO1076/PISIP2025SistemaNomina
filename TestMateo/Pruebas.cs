d6456baf20d02bd1f9c52727645323bdecc59a4d
MateoVasquezUisrael <e1752779908@uisrael.edu.ec>
2025-07-06 15:11:09 -0500

    Se modifico las entidades con el nuevo script, DTO añadido

--- TestMateo/Pruebas.cs
+++ TestMateo/Pruebas.cs
@@ -25,12 +25,12 @@ namespace TestMateo
         [Test]
          public async Task Test1()
         {
-            /*var puestoPrueba = new Puestos { idPuesto = 2 , PuestoNombre = "INGENIERO EN TELECOMUNICACIONES", PuestoSalario = (decimal)560.56, PuestoVacacionesCantidad = 40};
+            var puestoPrueba = new Puestos { PuestoNombre = "INGENIERO EN TELECOMUNICACIONES", PuestoSalario = (decimal)560.56, PuestoVacacionesCantidad = 40};
 
-            await _puestosServ.AddAsync( puestoPrueba );*/
+            await _puestosServ.AddAsync( puestoPrueba );
 
-            var prueba = await _empleServ.ObtenerContratoActivoEmpleados();
-            Console.WriteLine( prueba );
+            //var prueba = await _empleServ.ObtenerContratoActivoEmpleados();
+            //Console.WriteLine( prueba );
             Assert.Pass();
         }
 
