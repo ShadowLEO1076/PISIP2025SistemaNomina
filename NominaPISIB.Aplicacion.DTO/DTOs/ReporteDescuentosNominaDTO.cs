45259c7b1b23893d78920cd8044f02af14f8e7a1
ShadowLEO1076 <guillermoleoanrdocalvache@gmail.com>
2025-07-06 19:06:58 -0500

    ReporteDescuentosNominaDTO.cs

---
+++ NominaPISIB.Aplicacion.DTO/DTOs/ReporteDescuentosNominaDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NominaPISIB.Aplicacion.DTO.DTOs
+{
+    public class ReporteDescuentosNominaDTO
+    {
+        // Datos del empleado
+        public int IdEmpleado { get; set; }
+        public string NombresCompletos { get; set; }
+        public string NumeroIdentidad { get; set; }
+
+        // Datos de la nómina
+        public int Anio { get; set; }
+        public int Mes { get; set; }
+        public decimal SalarioBase { get; set; }
+        public decimal SueldoNeto { get; set; }
+
+        // Datos de los descuentos
+        public decimal TotalDescuentos { get; set; }
+        public List<string> DescripcionDescuentos { get; set; } = new();
+        public List<decimal> MontoDescuentos { get; set; } = new();
+    }
+}
+
